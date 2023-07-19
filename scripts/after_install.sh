2dcaa37ae86007232c9f28b61a76329eded210fd
Kevin Tuei <kevintuei89@gmail.com>
2023-07-20 00:15:32 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# navigate to app folder
-cd /app
-
-# install dependencies
+set -e
+cd /usr/share/nginx/html
 npm install
 npm run build
-cp -r build/* /var/www/html
 npm install pm2 -g
